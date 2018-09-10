6595caf192c1c83a6fc3084134a0deea1888bfc2
xugp <qhsoft@gmail.com>
2018-09-10 22:12:07 +0800

    add 1parts

---
+++ apps/caffe_cuda9/examples/cifar10/1parts/train_inception.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+pdsh -R ssh -w ^examples/cifar10/1parts/machinefile "pkill caffe_geeps"
+
+python scripts/duplicate.py examples/cifar10/1parts/inception_train_val.prototxt 1
+python scripts/duplicate.py examples/cifar10/1parts/inception_solver.prototxt 1
+
+LOG=output.txt
+
+if [ "$#" -eq 1 ]; then
+  mkdir $1
+  pwd > $1/pwd
+  git status > $1/git-status
+  git diff > $1/git-diff
+  cp examples/cifar10/1parts/train_inception.sh $1/.
+  cp examples/cifar10/1parts/inception_train_val.prototxt.template $1/.
+  cp examples/cifar10/1parts/inception_solver.prototxt.template $1/.
+  cp examples/cifar10/1parts/machinefile $1/.
+  cp examples/cifar10/1parts/ps_config_inception $1/.
+  LOG=$1/output.txt
+fi
+
+pdsh -R ssh -w ^examples/cifar10/1parts/machinefile "cd $(pwd) && ./build/tools/caffe_geeps train --solver=examples/cifar10/1parts/inception_solver.prototxt --ps_config=examples/cifar10/1parts/ps_config_inception --machinefile=examples/cifar10/1parts/machinefile --worker_id=%n" 2>&1 | tee ${LOG}
